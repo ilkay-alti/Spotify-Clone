92b2bc1752528e51d47e8d279e7f7990c6ecc1f5
ilkay Altınışık <ilkayalti@hotmail.com>
2022-01-03 19:19:11 +0100

    content update

---
+++ src/components/content/LastListen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const LastListen = () => {
+  return (
+    <div>
+      <NavLink to={"/library/playlist"}>
+        <div className="bg-[rgb(255,255,255,.3)] rounded-md h-[80px] min-w-[290px] max-w-[365px] ">
+          <div className="flex">
+            <img
+              src="https://i.scdn.co/image/ab67616d0000b2739cddf735c8cd47802cf9eba3 "
+              alt="a"
+              className="w-[80px]"
+            />
+
+            <div className=" p-[16px] font-bold leading-[24px]">
+              <p>Anka:The Return Of The Phoenix</p>
+            </div>
+          </div>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default LastListen;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import LastListen from ".././components/content/LastListen";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-[16px] py-[24px]">
+      <h2 className="font-extrabold text-[32px] leading-[36px] tracking-[-.04em] mb-[19px]">
+        Good evening
+      </h2>
+      <div class="grid grid-cols-4 gap-4">
+        <LastListen />
+        <LastListen />
+        <LastListen />
+        <LastListen />
+        <LastListen />
+        <LastListen />
+        <LastListen />
+        <LastListen />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
